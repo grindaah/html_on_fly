9c9852f0a6f3c466de56f4bed7362f17737d3889
Victor Lifagin <grindaah@gmail.com>
2014-03-21 23:28:39 +0400

    updated and improved version from Alexander

--- html_lib/urler.py
+++ html_lib/urler.py
@@ -3,34 +3,45 @@
 def urler(str):
     try:
         link, params = str.split("?", 1)
-        path = parse_path(link)
+        path, name = parse_path(link)
         try:
             req, metka = params.split("#", 1)
             try:
                 args = req.split("&")
-                return path, args, metka
+                return path, name, params_to_dict(args), metka
             except ValueError:
-                return path, req, metka
+                return path, name, req, metka
         except ValueError:
+            metka = ""
             try:
                 args = params.split("&")
-                return path, args
+                return path, name, params_to_dict(args), metka
             except ValueError:
-                return path, params
+                return path, name, params, metka
     except ValueError:
-        args = []
-        if str.count("#") > 0:
+        args = {}
+        try:
             link, metka = str.split("#", 1)
-        else:
-            link = str
+            path, name = parse_path(link)
+            return path, name, args, metka
+        except ValueError:
             metka = ""
-        path = parse_path(link)
-        return path, args, metka
+            path, name = parse_path(str)
+            return path, name, args, metka
 
 
 def parse_path(link):
-    link = link.split("/")[3:]
+    name = link.split("/")[-1:][0]
+    tree = link.split("/")[3:-1]
     path = ""
-    for i in link:
-        path += i
-    return path
+    for i in tree:
+        path += "/" + i
+    return path, name
+
+
+def params_to_dict(params):
+    d = {}
+    for i in params:
+        key, value = i.split("=")
+        d[key] = value
+    return d
