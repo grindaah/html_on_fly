07ce89249429db5cfd8e4725307ac83809ca307c
Victor Lifagin <grindaah@gmail.com>
2014-03-23 05:01:51 -0400

    remove old file and fix importing errors

--- html_lib/sqler.py
+++ html_lib/sqler.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 import sqlite3
 
 filename = 'my_first_db.sqlite'
@@ -16,3 +17,14 @@ def perform_request(base, request, params="1"):
     print rows
     return rows, request
 
+def add_row(Id, name, Price):
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+
+    try:
+        c.execute("INSERT INTO products VALUES ({id}, '{Name}', {price})".format(id = Id, Name = name, price = Price))
+    except sqlite3.IntegrityError:
+        print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_second))
+
+    conn.commit()
+    conn.close()
--- project.py
+++ project.py
@@ -1,14 +1,25 @@
 #coding utf-8
-import html_lib
-#import "html_lib/urler.py"
+import sys
+sys.path.append("./html_lib")
+
+from html_lib import urler
+from html_lib import sqler
+from html_lib import htmler
+
+from sys import argv
+
 sample = "http://localhost:8000/products"
 def __main__(options):
     if len(options) > 1:
         print "Some useless data, only one paramater needed"
 
-    req, param, label = urler.urler(options[0])
-    page_data, page_name = sql_getter.perform_request("my_first_db.sqlite", req)
+    print options
+    path, req, param, label = urler.urler(options[0])
+    page_data, page_name = sqler.perform_request("my_first_db.sqlite", req)
     htmler.htmler(page_data, page_name)
 
-if __name__ == 'main':
-    main(argv[1:])
+if __name__ == '__main__':
+    if len(argv) > 1:
+        __main__(argv[1:])
+    else:
+        __main__([sample])
