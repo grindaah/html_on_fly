b36c5cc37bcf211049c819d5a759db578be0a7ad
Victor Lifagin <grindaah@gmail.com>
2014-03-23 16:20:59 -0400

    slight changes of htmler, some text moved to constants and add bootstrap includes

--- html_lib/htmler.py
+++ html_lib/htmler.py
@@ -2,15 +2,16 @@
 import os
 
 page_data=((1,"Лодка",200),(2,"Машина",4000),(3,"Самолёт",450))
-
+html_header = '<html><head><title>{0}</title><link href="bs/css/bootstrap.min.css" rel="stylesheet"></head><body><table class="table">'
+html_close = "</table></body></html>"
 def htmler(page_data,page_name="Страница отсутствует",*args):
     
-    return_table = '';
+    return_table = '<th>ID</th><th>Name</th><th>Price</th>';
     for line in page_data:
         return_table+='<tr><td>'+str(line[0])+'</td><td>'+str(line[1])+'</td><td>'+str(line[2])+'</td></tr>'
 
-    open("sample.html", "w").write('<html><head><title>'+page_name+'</title></head><body><table style="border:solid 1px #000, width: 100px;">'+return_table+'</table></body></html>')
-    return '<html><head><title>'+page_name+'</title></head><body><table>'+return_table+'</table></body></html>'
+    open("sample.html", "w").write(html_header.format(page_name)+return_table+'<script src="bs/js/bootstrap.min.js"></script>'+html_close)
+    return html_header+return_table+html_close
 
 
 #print htmler(page_name,page_data)
